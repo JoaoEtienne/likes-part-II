f5ba2274fcb9eaefb2903906380c47e992c283d7
Joao Etienne <joaobaezetienne@gmail.com>
2023-02-25 17:29:03 -0800

    Adding likes files

---
+++ script.js
@@ -0,0 +1,30 @@
+var counter = 0;
+var neilsLikes = document.querySelector("#neil")
+
+function add1() {
+    counter ++;
+    neilsLikes.innerText = counter + " like(s)"
+    console.log(counter);
+}
+
+var count2 = 0;
+var nicholesLikes = document.querySelector("#nichole");
+
+function add2() {
+    count2 ++;
+    nicholesLikes.innerText = count2 + " like(s)"
+    console.log(count2);
+}
+
+var count3 = 0;
+var jimsLikes = document.querySelector("#jim");
+
+function add3() {
+    count3 ++;
+    jimsLikes.innerText = count3 + " like(s)"
+    console.log(count3);
+}
+
+
+
+
